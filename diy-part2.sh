d5b5d2c8a361083f8df93d280f8e4731efb186bd
h <78501382+oktoberdead@users.noreply.github.com>
2026-07-30 18:30:56 +0300

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
-
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+# Скачиваем и накладываем патч для поддержки Cudy WR1300 v4
 curl -L 'https://ozlabs.org' | git am
+
+# Принудительно создаем файл конфигурации .config под твою модель
+echo "CONFIG_TARGET_ramips=y" > .config
+echo "CONFIG_TARGET_ramips_mt7621=y" >> .config
+echo "CONFIG_TARGET_DEVICE_ramips_mt7621_DEVICE_cudy_wr1300-v4=y" >> .config
