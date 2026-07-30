a1118ae01877d39c2e1998df44f0e0c1c26d8c82
h <78501382+oktoberdead@users.noreply.github.com>
2026-07-31 00:32:07 +0300

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-# Скачиваем и накладываем патч для поддержки Cudy WR1300 v4
+# Накатываем патч для поддержки Cudy WR1300 v4
 curl -L 'https://ozlabs.org' | git am
 
-# Принудительно создаем файл конфигурации .config под твою модель
+# Жестко прописываем таргет, архитектуру и конкретное устройство
 echo "CONFIG_TARGET_ramips=y" > .config
 echo "CONFIG_TARGET_ramips_mt7621=y" >> .config
 echo "CONFIG_TARGET_DEVICE_ramips_mt7621_DEVICE_cudy_wr1300-v4=y" >> .config
+
+# Отключаем сборку всех остальных плат, чтобы не собирался дефолтный adslr
+echo "CONFIG_TARGET_MULTI_PROFILE=n" >> .config
+echo "CONFIG_TARGET_PER_DEVICE_ROOTFS=y" >> .config
